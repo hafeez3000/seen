06ce11f9ba88de737c31df20a3b29c4eb1663232
Tim Helfensdörfer <tim@visualappeal.de>
2014-04-21 20:44:46 +0200

    Fixed error when error status code is unknown

--- views/site/error.php
+++ views/site/error.php
@@ -8,14 +8,16 @@
 
 use yii\helpers\Html;
 
-$this->title[] = Yii::t('Error', 'Error {code}', ['code' => $exception->statusCode]);
+$statusCode = isset($exception->statusCode) ? $exception->statusCode : Yii::t('Error', 'Unknown');
+
+$this->title[] = Yii::t('Error', 'Error {code}', ['code' => $statusCode]);
 ?>
 <div id="error">
-	<h1><?php echo Html::encode(Yii::t('Error', 'Error {code}', ['code' => $exception->statusCode])); ?></h1>
+	<h1><?php echo Html::encode(Yii::t('Error', 'Error {code}', ['code' => $statusCode])); ?></h1>
 
 	<div class="alert alert-danger">
 		<?php if (!YII_DEBUG): ?>
-			<?php if ($exception->statusCode == 404): ?>
+			<?php if ($statusCode == 404): ?>
 				<?php echo Yii::t('Error', 'The site you were looking for does not exist! Please contact us if you think this was our mistake.') ?>
 			<?php else: ?>
 				<?php echo Yii::t('Error', 'There was an error while processing your request! We got informed and are trying to fix this error as soon as possible.') ?>
