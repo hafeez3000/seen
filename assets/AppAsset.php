43a6f294c71054a9592b99050ec8588a2808a553
Tim Helfensdörfer <tim@visualappeal.de>
2014-04-26 10:07:48 +0200

    Select2 asset only chooses languages that exists

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -8,6 +8,50 @@ class AppAsset extends AssetBundle
 	public $basePath = '@webroot';
 	public $baseUrl = '@web';
 
+	public $select2Languages = [
+		'ar',
+		'bg',
+		'ca',
+		'cs',
+		'da',
+		'de',
+		'el',
+		'es',
+		'et',
+		'eu',
+		'fa',
+		'fi',
+		'fr',
+		'gl',
+		'he',
+		'hr',
+		'hu',
+		'id',
+		'is',
+		'it',
+		'ja',
+		'ka',
+		'ko',
+		'lt',
+		'lv',
+		'mk',
+		'ms',
+		'nl',
+		'no',
+		'pl',
+		'pt',
+		'ro',
+		'rs',
+		'ru',
+		'sk',
+		'sv',
+		'th',
+		'tr',
+		'uk',
+		'vi',
+		'zh',
+	];
+
 	public $css = [
 		'css/app.min.css',
 	];
@@ -22,7 +66,7 @@ class AppAsset extends AssetBundle
 	];
 
 	public function init() {
-		if (Yii::$app->language != 'en')
+		if (Yii::$app->language != 'en' && in_array(Yii::$app->language, $this->select2Languages))
 			$this->js[] = 'components/select2/select2_locale_' . Yii::$app->language . '.js';
 
 		return parent::init();
