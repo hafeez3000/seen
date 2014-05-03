f11cc3fbbde08238dac9d95099ff202dc0253d33
Tim Helfensdörfer <tim@visualappeal.de>
2014-05-03 23:37:53 +0200

    Fixed season error where seasons with number 0 cannot be added

---
+++ migrations/m140503_212928_season_number_null.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Schema;
+use \yii\db\Migration;
+
+class m140503_212928_season_number_null extends Migration
+{
+	public function up()
+	{
+		$this->alterColumn('{{%season}}', 'number', 'smallint(5) unsigned DEFAULT NULL COMMENT "Number"');
+	}
+
+	public function down()
+	{
+		$this->alterColumn('{{%season}}', 'number', 'smallint(5) unsigned NOT NULL DEFAULT "0" COMMENT "Number"');
+
+		return true;
+	}
+}
