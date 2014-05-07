038d463b698ad935eeb355f975fbd1e24342be39
Tim Helfensdörfer <tim@visualappeal.de>
2014-05-07 10:13:20 +0200

    Fixed User model to match new findIdentityByAccessToken declaration, again

--- models/User.php
+++ models/User.php
@@ -137,7 +137,7 @@ class User extends ActiveRecord implements IdentityInterface
 	/**
 	 * @inheritdoc
 	 */
-	public static function findIdentityByAccessToken($token, $type)
+	public static function findIdentityByAccessToken($token, $type = null)
 	{
 		return null;
 	}
