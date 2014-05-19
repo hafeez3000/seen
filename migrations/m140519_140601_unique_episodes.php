70253f8ca776bb2035074a4e3ff0a7955d8a78d3
Tim Helfensdörfer <tim@visualappeal.de>
2014-05-19 16:20:49 +0200

    Do not drop themoviedb_id index on show table

--- migrations/m140519_140601_unique_episodes.php
+++ migrations/m140519_140601_unique_episodes.php
@@ -7,8 +7,6 @@ class m140519_140601_unique_episodes extends \yii\db\Migration
 {
     public function up()
     {
-        $this->dropIndex('themoviedb_id', '{{%show}}');
-
         $this->createIndex('show_themoviedb_id_language_id', '{{%show}}', [
             'themoviedb_id',
             'language_id',
@@ -28,10 +26,6 @@ class m140519_140601_unique_episodes extends \yii\db\Migration
     public function down()
     {
         $this->dropIndex('show_themoviedb_id_language_id', '{{%show}}');
-        $this->createIndex('show_themoviedb_id_number', '{{%show}}', [
-            'themoviedb_id',
-        ]);
-
         $this->dropIndex('season_show_id_number', '{{%season}}');
         $this->dropIndex('episode_season_id_number', '{{%episode}}');
     }
