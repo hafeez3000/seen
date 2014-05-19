c156bf0b374471168803b14547fc615a25c7c437
Tim Helfensdörfer <tim@visualappeal.de>
2014-05-19 16:17:18 +0200

    Ensure unique shows, seasons and episodes

---
+++ migrations/m140519_140601_unique_episodes.php
@@ -0,0 +1,38 @@
+<?php
+
+use \yii\db\Schema;
+use \yii\db\Migration;
+
+class m140519_140601_unique_episodes extends \yii\db\Migration
+{
+    public function up()
+    {
+        $this->dropIndex('themoviedb_id', '{{%show}}');
+
+        $this->createIndex('show_themoviedb_id_language_id', '{{%show}}', [
+            'themoviedb_id',
+            'language_id',
+        ], true);
+
+        $this->createIndex('season_show_id_number', '{{%season}}', [
+            'show_id',
+            'number',
+        ], true);
+
+        $this->createIndex('episode_season_id_number', '{{%episode}}', [
+            'season_id',
+            'number',
+        ], true);
+    }
+
+    public function down()
+    {
+        $this->dropIndex('show_themoviedb_id_language_id', '{{%show}}');
+        $this->createIndex('show_themoviedb_id_number', '{{%show}}', [
+            'themoviedb_id',
+        ]);
+
+        $this->dropIndex('season_show_id_number', '{{%season}}');
+        $this->dropIndex('episode_season_id_number', '{{%episode}}');
+    }
+}
