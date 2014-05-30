1ca89b72a9da5a2d5e435a26f75a585515b49ed2
Tim Helfensdörfer <tim@visualappeal.de>
2014-05-30 14:32:08 +0200

    Display notification if no person description is available and added table with person data like birth- and deathday

--- views/person/view.php
+++ views/person/view.php
@@ -6,6 +6,8 @@
 use \yii\helpers\Html;
 use \yii\helpers\Url;
 
+use \app\components\LanguageHelper;
+
 $this->title[] = $person->name;
 $this->title[] = Yii::t('Person/View', 'Persons');
 ?>
@@ -22,7 +24,45 @@ $this->title[] = Yii::t('Person/View', 'Persons');
 
 			<?php if (!empty($person->biography)): ?>
 				<p><?php echo Html::encode($person->biography); ?></p>
+            <?php else: ?>
+                <?php if ($person->updated_at !== null): ?>
+                    <div class="alert alert-info"><?php echo Yii::t('Person/View', 'Currently theire is no description for this person available. Help by <a href="{url}">creating one</a> at The Movie Database.', [
+                        'url' => 'https://www.themoviedb.org/person/' . $person->id,
+                    ]); ?></div>
+                <?php else: ?>
+                    <div class="alert alert-info"><?php echo Yii::t('Person/View', 'Currently theire is no description for this person available.'); ?></div>
+                <?php endif; ?>
 			<?php endif; ?>
+
+            <?php if (!empty($person->birthday) || !empty($perosn->deathday) || !empty($person->homepage)): ?>
+                <div class="row">
+                    <div class="col-md-6">
+                        <table class="table table-stripped">
+                            <tbody>
+                                <?php if (!empty($person->birthday)): ?>
+                                    <tr>
+                                        <td><strong><?php echo Yii::t('Person/View', 'Birthday'); ?></strong></td>
+                                        <td><?php echo LanguageHelper::date(strtotime($person->birthday)); ?></td>
+                                    </tr>
+                                <?php endif; ?>
+                                <?php if (!empty($person->deathday)): ?>
+                                    <tr>
+                                        <td><strong><?php echo Yii::t('Person/View', 'Deathday'); ?></strong></td>
+                                        <td><?php echo LanguageHelper::date(strtotime($person->deathday)); ?></td>
+                                    </tr>
+                                <?php endif; ?>
+                                <?php if (!empty($person->homepage)): ?>
+                                    <tr>
+                                        <td><strong><?php echo Yii::t('Person/View', 'Homepage'); ?></strong></td>
+                                        <td><a href="<?php echo Html::encode($person->homepage); ?>"><?php echo Html::encode($person->homepage); ?></td>
+                                    </tr>
+                                <?php endif; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            <?php endif; ?>
+
 
 			<?php if (count($movies) > 0): ?>
 				<h2><?php echo Yii::t('Person/View', 'Movies with/from {name}', ['name' => $person->name]); ?></h2>
