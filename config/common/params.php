9a1b1c42f7edc3b3a327bbe2bdba7ce1925e4749
Tim Helfensdörfer <tim@visualappeal.de>
2014-09-10 11:16:26 +0200

    Removed default params

--- config/common/params.php
+++ config/common/params.php
@@ -1,42 +1,4 @@
 <?php
 
 return [
-	'themoviedb' => [
-		'key' => '',
-		'url' => '',
-		'image_url' => 'https://image.tmdb.org/t/p/',
-	],
-	'email' => [
-		'admin' => 'admin@seenapp.com',
-		'system' => 'noreply@seenapp.com',
-		'mandrill' => [
-			'baseUrl' => 'https://mandrillapp.com/api/1.0/',
-			'apikey' => '',
-			'globalMergeVars' => [
-			],
-		],
-	],
-	'mailchimp' => [
-		'apikey' => '',
-		'listid' => '',
-	],
-	'salt' => '123456',
-	'lang' => [
-		'en' => [
-			'datetime' => 'Y\\\m\\\d H:i',
-			'date' => 'Y\\\m\\\d',
-			'decPoint' => '.',
-			'thousandsSep' => ',',
-		],
-	],
-	'oauth' => [
-		'facebook' => [
-			'key' => '',
-			'secret' => '',
-		]
-	],
-	'redis' => [
-		'host' => '127.0.0.1',
-		'port' => 6379,
-	],
 ];
