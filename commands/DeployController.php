5390747b18b69119a96c8c6269e8bce7147e34a5
Tim Helfensdörfer <tim@visualappeal.de>
2014-09-21 10:57:57 +0200

    Fixed cache flush after deployment

--- commands/DeployController.php
+++ commands/DeployController.php
@@ -23,7 +23,7 @@ class DeployController extends Controller
 			'composer install --optimize-autoloader --no-dev',
 			'./yii migrate --interactive=0',
 			'grunt build_production',
-			'./yii cache/flush-all',
+			'./yii cache/flush',
 			'rm -f .maintenance',
 		], function($line) {
 			echo $line;
