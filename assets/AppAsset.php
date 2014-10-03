f639434d16a41800599ed97745d8e16391be5c17
Tim Helfensdörfer <tim@visualappeal.de>
2014-10-03 12:43:34 +0200

    Removed select2 localization.

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -8,50 +8,6 @@ class AppAsset extends AssetBundle
 	public $basePath = '@webroot';
 	public $baseUrl = '@web';
 
-	public $select2Languages = [
-		'ar',
-		'bg',
-		'ca',
-		'cs',
-		'da',
-		'de',
-		'el',
-		'es',
-		'et',
-		'eu',
-		'fa',
-		'fi',
-		'fr',
-		'gl',
-		'he',
-		'hr',
-		'hu',
-		'id',
-		'is',
-		'it',
-		'ja',
-		'ka',
-		'ko',
-		'lt',
-		'lv',
-		'mk',
-		'ms',
-		'nl',
-		'no',
-		'pl',
-		'pt',
-		'ro',
-		'rs',
-		'ru',
-		'sk',
-		'sv',
-		'th',
-		'tr',
-		'uk',
-		'vi',
-		'zh',
-	];
-
 	public $css = [
 		'css/app.min.css',
 	];
@@ -64,11 +20,4 @@ class AppAsset extends AssetBundle
 	public $jsOptions = [
 		'position' => \yii\web\View::POS_HEAD
 	];
-
-	public function init() {
-		if (Yii::$app->language != 'en' && in_array(Yii::$app->language, $this->select2Languages))
-			$this->js[] = '../vendor/bower/select2/select2_locale_' . Yii::$app->language . '.js';
-
-		return parent::init();
-	}
 }
