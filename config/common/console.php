35763df3532951e8a3ac16f83397eafa31f94d11
Tim Helfensdörfer <tim@visualappeal.de>
2015-01-06 10:43:47 +0100

    Removed bugsnag module

--- config/common/console.php
+++ config/common/console.php
@@ -3,5 +3,4 @@
 $config['controllerNamespace'] = 'app\commands';
 
 $config['bootstrap'][] = 'log';
-$config['bootstrap'][] = 'app\components\bootstrap\BugsnagBootstrap';
 $config['bootstrap'][] = 'app\components\bootstrap\EventBootstrap';
