03ca75290716a40799df1185296552d59832debc
Tim Helfensdörfer <thelfensdrfer@gmail.com>
2015-02-03 10:16:50 +0100

    Added missing config params

--- config/testing/params.php
+++ config/testing/params.php
@@ -8,9 +8,10 @@ return [
 	],
 	'email' => [
 		'admin' => 'admin@seenapp.com',
+		'system' => 'no-reply@seenapp.com',
 		'mandrill' => [
-			'baseUrl' => '',
-			'apikey' => 'test',
+			'baseUrl' => 'https://mandrillapp.com/api/1.0/',
+			'apikey' => '6a7b5efb-e419-488f-b649-3afc33289439',
 			'globalMergeVars' => [
 			],
 		],
@@ -21,6 +22,25 @@ return [
 	],
 	'salt' => '123456',
 	'lang' => [
-		'default' => 'en',
+		'default' => 'en-US',
+		'default_iso' => 'en',
+		'en' => [
+			'datetime' => 'd/m/Y H:i',
+			'date' => 'd/m/Y',
+			'decPoint' => '.',
+			'thousandsSep' => ',',
+		],
+		'en-US' => [
+						'datetime' => 'd/m/Y H:i',
+						'date' => 'd/m/Y',
+						'decPoint' => '.',
+						'thousandsSep' => ',',
+				],
+		'de' => [
+			'datetime' => 'd.m.Y H:i',
+			'date' => 'd.m.Y',
+			'decPoint' => ',',
+			'thousandsSep' => '.',
+		],
 	],
 ];
