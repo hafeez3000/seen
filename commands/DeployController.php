92d039f35e469af225696a0d6ea3687bd9edfe80
Tim Helfensdörfer <tim@visualappeal.de>
2015-03-12 16:50:25 +0100

    Optimized composer install for deployment

--- commands/DeployController.php
+++ commands/DeployController.php
@@ -20,7 +20,7 @@ class DeployController extends Controller
 			'touch .maintenance',
 			'git fetch --all',
 			'git reset --hard origin/master',
-			'composer install --optimize-autoloader --no-dev',
+			'composer install --optimize-autoloader --no-dev --prefer-dist',
 			'./yii migrate --interactive=0',
 			'grunt build_production',
 			'./yii cache/flush cache --interactive=0',
