a72666d838de0ff5c6b1b01925c005c8f16cc8a7
Tim Helfensdörfer <tim@visualappeal.de>
2015-09-18 08:57:43 +0200

    Created comments for mixpanel class and excluded bots

--- components/YiiMixpanel.php
+++ components/YiiMixpanel.php
@@ -7,14 +7,41 @@ class YiiMixpanel
 {
 	protected static $mp = null;
 
-	protected static function init()
+	/**
+	 * Check if current user is eventually a bot.
+	 *
+	 * @return boolean
+	 */
+	protected static function isBot()
 	{
-		if (isset(Yii::$app->params['mixpanel']) && self::$mp === null)
-			self::$mp =  Mixpanel::getInstance(Yii::$app->params['mixpanel']);
-		else
-			return null;
+		if (isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/bot|crawl|slurp|spider/i', $_SERVER['HTTP_USER_AGENT'])) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
+	/**
+	 * Init the mixpanel component
+	 *
+	 * @return [type]
+	 */
+	protected static function init()
+	{
+		if (isset(Yii::$app->params['mixpanel']) && !self::isBot() && self::$mp === null)
+			self::$mp =  Mixpanel::getInstance(Yii::$app->params['mixpanel']);
+		else
+			self::$mp = null;
+	}
+
+	/**
+	 * Track an event.
+	 *
+	 * @param string $event
+	 * @param array $data
+	 *
+	 * @return void
+	 */
 	public static function track($event, array $data = array())
 	{
 		self::init();
@@ -24,6 +51,14 @@ class YiiMixpanel
 		self::$mp->track($event, $data);
 	}
 
+	/**
+	 * Register global variables.
+	 *
+	 * @param string $property
+	 * @param array $data
+	 *
+	 * @return void
+	 */
 	public static function register($property, array $data)
 	{
 		self::init();
@@ -33,6 +68,13 @@ class YiiMixpanel
 		self::$mp->registerAll($property, $data);
 	}
 
+	/**
+	 * Identify a user.
+	 *
+	 * @param string $id
+	 *
+	 * @return void
+	 */
 	public static function identify($id)
 	{
 		self::init();
@@ -42,6 +84,14 @@ class YiiMixpanel
 		self::$mp->identify($id);
 	}
 
+	/**
+	 * Connect to user accounts.
+	 *
+	 * @param string $oldId
+	 * @param string $newId
+	 *
+	 * @return void
+	 */
 	public static function createAlias($oldId, $newId)
 	{
 		self::init();
