fd6ad6287b784f54feb0c9e38937768a9aa3897e
Tim Helfensdörfer <tim@visualappeal.de>
2015-12-10 13:35:36 +0100

    503 http code if app is in maintenance mode

--- components/bootstrap/MaintenanceBootstrap.php
+++ components/bootstrap/MaintenanceBootstrap.php
@@ -13,6 +13,9 @@ class MaintenanceBootstrap implements BootstrapInterface
 	{
 		if (file_exists($app->basePath . '/.maintenance')) {
 			// Application is in maintenance mode
+			header('HTTP/1.1 503 Service Temporarily Unavailable');
+			header('Status: 503 Service Temporarily Unavailable');
+			header('Retry-After: 300');
 			$app->end(0, new Response([
 				'content' => Yii::t('Maintenance', 'The application is currently updating, please try again in a few seconds.')
 			]));
